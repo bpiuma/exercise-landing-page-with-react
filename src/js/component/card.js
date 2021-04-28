020443c8efec6e30943a719bfab2199b88717175
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-28 12:38:59 +0000

    Mejoras en las card

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,116 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export function Card() {
+export function Card(props) {
 	return (
-		<div className="container-fluid">
-			<div className="container ">
-				<div className="row  justify-content-center">
-					<div className="col-12 col-sm-6 col-md-3 my-3 my-md-0">
-						<div className="card p-0 m-0">
-							<img
-								src="https://placeimg.com/640/480/any"
-								className="card-img-top w-100 "
-								alt="..."
-							/>
-							<div className="card-body">
-								<h5 className="card-title text-center">
-									Card title
-								</h5>
-								<p className="card-text text-center">
-									This is a longer card with supporting text
-									below as a natural lead-in to additional
-									content.
-								</p>
-							</div>
-							<div className="card-footer d-flex justify-content-center">
-								<button
-									type="button"
-									className="btn btn-primary">
-									Find Out More!
-								</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-12 col-sm-6 col-md-3 my-3 my-md-0">
-						<div className="card p-0 m-0">
-							<img
-								src="https://placeimg.com/640/480/any"
-								className="card-img-top w-100 "
-								alt="..."
-							/>
-							<div className="card-body">
-								<h5 className="card-title text-center">
-									Card title
-								</h5>
-								<p className="card-text text-center">
-									This is a longer card with supporting text
-									below as a natural lead-in to additional
-									content.
-								</p>
-							</div>
-							<div className="card-footer d-flex justify-content-center">
-								<button
-									type="button"
-									className="btn btn-primary">
-									Find Out More!
-								</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-12 col-sm-6 col-md-3 my-3 my-md-0">
-						<div className="card p-0 m-0">
-							<img
-								src="https://placeimg.com/640/480/any"
-								className="card-img-top w-100 "
-								alt="..."
-							/>
-							<div className="card-body">
-								<h5 className="card-title text-center">
-									Card title
-								</h5>
-								<p className="card-text text-center">
-									This is a longer card with supporting text
-									below as a natural lead-in to additional
-									content.
-								</p>
-							</div>
-							<div className="card-footer d-flex justify-content-center">
-								<button
-									type="button"
-									className="btn btn-primary">
-									Find Out More!
-								</button>
-							</div>
-						</div>
-					</div>
-					<div className="col-12 col-sm-6 col-md-3 my-3 my-md-0">
-						<div className="card p-0 m-0">
-							<img
-								src="https://placeimg.com/640/480/any"
-								className="card-img-top w-100 "
-								alt="..."
-							/>
-							<div className="card-body">
-								<h5 className="card-title text-center">
-									Card title
-								</h5>
-								<p className="card-text text-center">
-									This is a longer card with supporting text
-									below as a natural lead-in to additional
-									content.
-								</p>
-							</div>
-							<div className="card-footer d-flex justify-content-center">
-								<button
-									type="button"
-									className="btn btn-primary">
-									Find Out More!
-								</button>
-							</div>
-						</div>
-					</div>
+		<div className="col-12 col-sm-6 col-md-3 my-3 my-md-0">
+			<div className="card p-0 m-0">
+				<img
+					src={props.imagen}
+					className="card-img-top w-100 "
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title text-center">{props.titulo}</h5>
+					<p className="card-text text-center">{props.descripcion}</p>
+				</div>
+				<div className="card-footer d-flex justify-content-center">
+					<a href={props.buttonURL} className="btn btn-primary">
+						Click
+					</a>
 				</div>
 			</div>
 		</div>
 	);
 }
+Card.propTypes = {
+	titulo: PropTypes.string,
+	descripcion: PropTypes.string,
+	imagen: PropTypes.string,
+	buttonURL: PropTypes.string
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,11 +6,62 @@ import { Card } from "./card";
 import { Footer } from "./footer";
 //create your first component
 export function Home() {
+	let personas = [
+		{
+			titulo: "Luis Suarez",
+			descripcion:
+				"Luis Alberto Suárez Díaz es un futbolista uruguayo. Juega como delantero y su equipo actual es el Atlético de Madrid de la Primera División de España. Es internacional absoluto con la selección de fútbol de Uruguay, de la cual es el máximo goleador histórico. También posee ciudadanía italiana.",
+			imagen:
+				"https://depor.com/resizer/a2hHLWCJk_1JWU3szMbQp9sc8_Y=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/33LT2MGLJRHIVNTLWFM2CJPDDI.jpg",
+			buttonURL:
+				"https://es.wikipedia.org/wiki/Luis_Su%C3%A1rez_(futbolista)"
+		},
+		{
+			titulo: "Rubén Rada",
+			descripcion:
+				"Sus primeros pasos en la música fueron a los diez años como integrante de la comparsa de negros y lubolos Morenada2​. Su primer seudónimo fue Zapatito, originado en que a esa edad ya calzaba zapatos talla europea 434​. De los 15 a los 16 años salía en una murga que se ",
+			imagen:
+				"https://mus.uy/landing/wp-content/uploads/2016/04/rada.jpg",
+			buttonURL: "https://es.wikipedia.org/wiki/Rub%C3%A9n_Rada"
+		},
+		{
+			titulo: "Messi",
+			descripcion:
+				"Lionel Andrés Messi Cuccittini (Rosario, Santa Fe; 24 de junio de 1987), conocido como Leo Messi,7​ es un futbolista argentino que juega como delantero o centrocampista.8​9​ Ha desarrollado toda su carrera en el F. C. Barcelona de la Primera División de España y en la selección de Argentina, equipos de los que e",
+			imagen:
+				"https://atalayar.com/sites/default/files/styles/foto_/public/noticias/Atalayar_Leo%20Messi%20Barcelona_0.jpg?itok=moY7JlzF",
+			buttonURL: "https://es.wikipedia.org/wiki/Lionel_Messi"
+		},
+		{
+			titulo: "Carlos Gardel",
+			descripcion:
+				"Carlos Gardel fue un cantante, compositor y actor de cine uruguayo. Es el más conocido representante (del género) en la historia del tango. Iniciador y máximo exponente del tango canción,3​ fue uno de los intérpretes más importantes de la música popular mundial en la primera mitad del siglo xx,4​ por la calidad de su voz, por la cantidad de discos vendidos (como cantante y como compositor)",
+			imagen:
+				"https://upload.wikimedia.org/wikipedia/commons/f/fa/Gardel_color.jpg",
+			buttonURL: "https://es.wikipedia.org/wiki/Carlos_Gardel"
+		}
+	];
+
 	return (
 		<div>
 			<NavBar />
 			<Jumbotron />
-			<Card />
+			<div className="container ">
+				<div className="row  justify-content-center">
+					{personas.map((element, i) => {
+						return (
+							<Card
+								key={i}
+								titulo={element.titulo}
+								descripcion={element.descripcion}
+								imagen={element.imagen}
+								buttonURL={element.buttonURL}
+							/>
+						);
+					})}
+				</div>
+			</div>
+
 			<Footer />
 		</div>
 	);
